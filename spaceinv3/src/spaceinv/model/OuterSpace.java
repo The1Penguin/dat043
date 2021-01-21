c97e7ce777344eabf3ba15ca55d39adf0f82e281
The1Penguin <jfuhr01@gmail.com>
2021-01-21 13:10:09 +0100

    Basic file of all labs except for Mexico which I have started completing.

---
+++ spaceinv3/src/spaceinv/model/OuterSpace.java
@@ -0,0 +1,12 @@
+package spaceinv.model;
+
+import static spaceinv.model.SI.GAME_WIDTH;
+import static spaceinv.model.SI.OUTER_SPACE_HEIGHT;
+
+/*
+    Used to check if projectiles from gun have left our world
+    Non visible class
+ */
+public class OuterSpace {
+
+}
